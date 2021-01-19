a830468e608b8627dadac4f0236c525d2884b89b
Ralph Studer <51389896+ralphjstuder@users.noreply.github.com>
2021-01-19 10:01:19 -0700

    Update pr_of_incl_excl.py

--- pr_of_incl_excl.py
+++ pr_of_incl_excl.py
@@ -4,36 +4,38 @@ objects = int(input("Enter the number of objects for distribution: "))
 nodes = int(input("Enter the number of nodes: "))
 limit = int(input("Enter the maximum number of objects per node: "))
 
-def nCr(n, k):
+def nCr(n, r):
+    # Number of combinations for choosing r out of n
     f = math.factorial
-    return f(abs(n)) // f(abs(k)) // f(abs(n - k))
+    return f(abs(n)) // f(abs(r)) // f(abs(n - r))
 
 def inclusion_exclusion(x):
-    #Exclude cases that are double counted
-    i = 0
+    # Exclude cases that are double counted
+    count = 0
     plus = []
     minus = []
-    while i <= len(value) - 1:
-        plus.append(value[i])
+    while count <= len(value) - 1:
+        plus.append(value[count])
         try:
-            minus.append(value[i + 1])
+            minus.append(value[count + 1])
         except IndexError:
             pass
         i += 2
     print(x-(sum(plus) - sum(minus)))
 
-def pie(objects, recipients, limit):
-    #Stars and bars
-    bars = recipients - 1
+def principle_of_inclusion_exclusion(obj, node, lim):
+    # Stars and bars
+    bars = nodes - 1
     total = int(nCr(objects + bars, bars))
-    #All cases where limit is exceeded
+    
+    # All cases where limit is exceeded
     remain = objects+bars
-    i = 1
+    count = 1
     while remain >= bars:
         remain = remain - (limit + 1)
-        incl_exc = int(nCr(recipients, i) * nCr(remain, bars))
+        incl_exc = int(nCr(nodes, count) * nCr(remain, bars))
         value.append(incl_exc)
-        i += 1
+        count += 1
     inclusion_exclusion(total)
 
-pie(objects, recipients, limit)
+principle_of_inclusion_exclusion(objects, nodes, limit)
